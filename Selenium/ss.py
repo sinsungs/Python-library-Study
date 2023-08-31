62a537844ba62bdbe7d7c7cfd6ac0ee7dbdf09af
ss <rkdtlstjd123@naver.com>
2023-08-31 21:38:28 +0900

    [Feat] Selenium 테스트

---
+++ Selenium/ss.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# import time
+
+options = Options()
+options.add_experimental_option('detach', True) # 브라우저 바로 닫힘 방지
+
+service = Service(ChromeDriverManager().install())
+
+# # print(servicer)
+
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('https://naver.com')
+
